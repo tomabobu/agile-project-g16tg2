2fbda63df74c728dddc896d3956a05e6b7194ed2
Bogdan  Toma <bogdan.toma@tesagon.com>
2021-01-20 22:15:43 +0200

    More work on the custom cake page

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
  // When the user scrolls the page, execute myFunction
- window.onscroll = function() {
-	myFunction()
-  };
+//  window.onscroll = function() {
+// 	myFunction()
+//   };
 
-  // Get the navbar
-  var navbar = document.getElementById("navbar");
+//   // Get the navbar
+//   var navbar = document.getElementById("navbar");
 
-  // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
+//   // Get the offset position of the navbar
+//   var sticky = navbar.offsetTop;
 
-  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function myFunction() {
-	if (window.pageYOffset >= sticky) {
-	  navbar.classList.add("sticky")
-	} else {
-	  navbar.classList.remove("sticky");
-	}
-  }
+//   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+//   function myFunction() {
+// 	if (window.pageYOffset >= sticky) {
+// 	  navbar.classList.add("sticky")
+// 	} else {
+// 	  navbar.classList.remove("sticky");
+// 	}
+//   }
+
+  $(document).ready( function() {
+    $(window).scroll(function() {
+		console.log('trece');
+        var scrollTop = $(window).scrollTop();
+        var divam = 7;
+        $(".custom-cake-form-background").css({
+            "background-position":"0px -"+scrollTop/divam+"px"
+        });
+        // $(".red").css({
+        //     "margin-top":"-"+scrollTop+"px"
+        // });
+    });
+});
