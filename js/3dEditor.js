d4989ee22a781e589f27d4081e44a4cdd518f305
Bogdan  Toma <bogdan.toma@tesagon.com>
2021-02-04 23:07:21 +0200

    Generate profile curves for the top and bottom cake borders

--- js/3dEditor.js
+++ js/3dEditor.js
@@ -7,6 +7,12 @@ var instances = [];
 var textures = [];
 var materials = [];
 
+var sizes = {
+	'small' : [0.65, 0.75, 0.65],
+	'medium':[0.85, 0.85, 0.85],
+	'big':[1.1, 0.9, 1.1]
+};
+
 var options = {
 	'step': 1,
 	'initialSetup' : true,
@@ -19,6 +25,9 @@ var options = {
 	'orbitPoint' : new THREE.Vector3( 0, 50 / 2, 0 ),
 	'materialsUsed' : [],
 	'baseColor' : 0xFFFFFF,
+	'topBorder' : null,
+	'bottomBorder' : null,
+	'cakeSize' : 'medium',
 };
 
 
@@ -31,7 +40,7 @@ function init() {
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / (2*window.innerHeight), 1, 100000 );
 	camera.position.set( 0, 200, 300 );
-	//TODO set limits on camera rotation
+
 
 	// SCENE
 
@@ -124,6 +133,8 @@ function init() {
 						child.material = materials['dafaultLambert'];
 					})
 				}
+				cakeModels[cakeName].topBorder = createCurve(cakeName, 'top');
+				cakeModels[cakeName].bottomBorder = createCurve(cakeName, 'bottom');
 
 				if ( cakeModels[cakeName].isMesh ) {
 					cakeModels[cakeName].castShadow = true;
@@ -143,6 +154,7 @@ function init() {
 					// if(tableModel.material.map) tableModel.material.map.anisotropy = 1;
 				}
 			}
+
 		});
 		updateEditor();
 		// setBaseScale(1);
@@ -184,6 +196,43 @@ function render() {
 	renderer.render( scene, camera );
 }
 
+function addBorderInstances(position) {
+	if (position == 'top') {
+		curve = cakeModels[options.baseCake].topBorder;
+	} else {
+		curve = cakeModels[options.baseCake].bottomBorder;
+	}
+	numPoints = 0;
+	if (options.cakeSize == 'small') {
+		numPoints = 10;
+	}
+	if (options.cakeSize == 'medium') {
+		numPoints = 20;
+	}
+	if (options.cakeSize == 'big') {
+		numPoints = 30;
+	}
+	const points = curve.getPoints(numPoints);
+	const geometry = new THREE.BufferGeometry().setFromPoints( points );
+	const material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
+
+	// Create the final object to add to the scene
+	const curveObject = new THREE.Line( geometry, material );
+	curveObject.rotateOnWorldAxis( new THREE.Vector3(1,0,0), Math.PI/2);
+	if (position == 'top') {
+		translateMatrix =  new THREE.Matrix4().makeTranslation(0, 50, 0 );
+	} else {
+		translateMatrix =  new THREE.Matrix4().makeTranslation(0, 1, 0 );
+	}
+
+	baseScale = options.baseMatrix.clone();
+	baseScale.multiply(translateMatrix);
+	curveObject.applyMatrix4(baseScale);
+
+	//don't add curve to the scene, add the instances
+	scene.add(curveObject);
+}
+
 function updateEditor() {
 	//removing all cakes
 	for (const cake in cakeModels) {
@@ -206,6 +255,14 @@ function updateEditor() {
 			cakeModels[options.baseCake].applyMatrix4(cakeModels[options.baseCake].matrixWorld.invert())
 			cakeModels[options.baseCake].applyMatrix4(options.baseMatrix.clone());
 			scene.add(cakeModels[options.baseCake]);
+			if (options.topBorder) {
+				//add top border for base cake
+				addBorderInstances('top');
+			}
+			if (options.bottomBorder) {
+				addBorderInstances('bottom');
+				//add bottom border for base cake
+			}
 		}
 
 		if (options.numberOfTiers > 1) {
@@ -261,8 +318,9 @@ function setNumberOfTiers(number) {
 	updateEditor();
 }
 
-function setBaseScale(x,y,z) {
-	options.baseMatrix =  new THREE.Matrix4().makeScale(x, y, z);
+function setBaseScale(size) {
+	options.cakeSize = size;
+	options.baseMatrix =  new THREE.Matrix4().makeScale(sizes[size][0], sizes[size][1], sizes[size][2]);
 	options.step = 4;
 	updateEditor();
 }
@@ -352,13 +410,87 @@ function setMaterialToAllBaseGeoms(firstMaterialName, secondMaterialName= false)
 			if (cakeModels[cake].children.length) {
 				cakeModels[cake].children[1].material = materials[secondMaterialName];
 				options['materialsUsed'].push(secondMaterialName);
-				console.log(options['materialsUsed']);
 			}
 		}
 	}
 }
 
 
+function roundedRect( ctx, x, y, width, height, radius ) {
+	ctx.moveTo( x, y + radius );
+	ctx.lineTo( x, y + height - radius );
+	ctx.quadraticCurveTo( x, y + height, x + radius, y + height );
+	ctx.lineTo( x + width - radius, y + height );
+	ctx.quadraticCurveTo( x + width, y + height, x + width, y + height - radius );
+	ctx.lineTo( x + width, y + radius );
+	ctx.quadraticCurveTo( x + width, y, x + width - radius, y );
+	ctx.lineTo( x + radius, y );
+	ctx.quadraticCurveTo( x, y, x, y + radius );
+}
+
+function createCurve(type, position) {
+	if (type == 'Cake_round') {
+		l1 = 111;
+		l2 = 111;
+	}
+	if (type == 'Cake_square') {
+		l1 = 200;
+		l2 = 200;
+	}
+	if (type == 'Cake_sheet') {
+		l1 = 240;
+		l2 = 160;
+	}
+	if (position == 'top') {
+		l1 = l1 * 0.95;
+		l2 = l2 * 0.95;
+	}
+	if (type == 'Cake_round') {
+		curve = new THREE.EllipseCurve(
+			0,  0,            // ax, aY
+			l1, l2,           // xRadius, yRadius
+			0,  2 * Math.PI,  // aStartAngle, aEndAngle
+			false,            // aClockwise
+			90                 // aRotation
+		);
+		console.log(curve);
+	} else if (type == 'Cake_square' || type == 'Cake_sheet') {
+		curve = new THREE.Shape();
+		roundedRect(curve, -l1/2, -l2/2, l1, l2, 10 );
+	}
+
+	// const points = curve.getPoints( 50 );
+	// const geometry = new THREE.BufferGeometry().setFromPoints( points );
+	// const material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
+
+	// // Create the final object to add to the scene
+	// const curveObject = new THREE.Line( geometry, material );
+	// curveObject.rotateOnWorldAxis( new THREE.Vector3(1,0,0), Math.PI/2);
+	// curveObject.applyMatrix4(options.baseMatrix.clone());
+	// if (position == 'bottom') {
+	// 	curveObject.translateZ(-1);
+	// }
+	// if (position == 'top') {
+	// 	curveObject.translateZ(-43);
+	// }
+	return curve;
+}
+
+function setBorder(type, position) {
+	if (position == 'top') {
+		options.topBorder = type;
+	}
+	if (position == 'bottom') {
+		options.bottomBorder = type;
+	}
+	updateEditor();
+}
+
+
+
+
+
+//TODO set limits on camera rotation
 //use geom instead of instances for base geoms to apply UV offsets for tier variation
 //preload materials before switching it (chrome material shown while loading model)
 //add stereo option
